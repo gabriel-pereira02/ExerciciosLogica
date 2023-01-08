726f0ef9a620b3a8938fa3bcf2967c1a81208192
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-08 17:05:33 -0300

    Adiciona Ex06

---
+++ EstruturaRepeticao/Ex06/Ex06/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Pede para o usuário digitar o número e imprime seus divisores
+             */
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                if(n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
