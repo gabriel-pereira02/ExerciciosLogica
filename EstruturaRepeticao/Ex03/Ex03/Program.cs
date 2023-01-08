8d3b816c275b643ffffcde6eef5d9c0449a403cf
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-08 16:49:06 -0300

    Adiciona Ex03

---
+++ EstruturaRepeticao/Ex03/Ex03/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = int.Parse(Console.ReadLine());
+            int i = 0;
+            int o = 0;
+            for (int contador = 0; contador < x; contador++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n >= 10 && n <= 20)
+                {
+                    i++;
+                }
+                else
+                {
+                    o++;
+                }
+            }
+            Console.WriteLine($"{i} in");
+            Console.WriteLine($"{o} out");
+        }
+    }
+}
