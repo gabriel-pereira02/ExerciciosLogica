1edb3584bf588e8dbd5d337725625dde26c6a986
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-08 17:37:36 -0300

    Adiciona Ex01 de Estrutura Condicional

---
+++ EstruturaCondicional/Ex01/Ex01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Pede para o usuário digitar dois números e verifica se eles são múltiplos
+             */
+            string[] numeros = Console.ReadLine().Split();
+            int num1 = int.Parse(numeros[0]);
+            int num2 = int.Parse(numeros[1]);
+            if(num1%num2 == 0 || num2%num1 == 0)
+            {
+                Console.WriteLine("Múltiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não Múltiplos");
+            }
+        }
+    }
+}
