2ef9248f5d1524bc2ce5f42d27669fca0b98046e
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-08 16:56:16 -0300

    Adiciona Ex04

---
+++ EstruturaRepeticao/Ex04/Ex04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Calcula a média ponderada entre três números 
+            */
+            
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] arr = Console.ReadLine().Split();
+                float a = float.Parse(arr[0]);
+                float b = float.Parse(arr[1]);
+                float c = float.Parse(arr[2]);
+
+                float resultado = (a * 2 + b * 3 + c * 5)/10;
+                Console.WriteLine($"{resultado}");
+            }
+        }
+    }
+}
