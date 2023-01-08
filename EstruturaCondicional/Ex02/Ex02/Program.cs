4bbb829bdfd6bf0ba62b0f57432d4d739acea29e
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-08 17:41:59 -0300

    Adiciona Ex02 de estrutura condicional

---
+++ EstruturaCondicional/Ex02/Ex02/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Pede ao usuário uma coordenada qualquer e imprime na tela o quadrande que esse ponto está no plano cartesiano
+             */
+            string[] coord = Console.ReadLine().Split();
+            float coordX = float.Parse(coord[0]);
+            float coordY = float.Parse(coord[1]);
+            if (coordX == 0 && coordY == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (coordX == 0 && coordY != 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            else if(coordX != 0 && coordY == 0) 
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if(coordX > 0 && coordY > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (coordX > 0 && coordY < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else if (coordX < 0 && coordY > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else
+            {
+                Console.WriteLine("Q3");
+            }
+        }
+    }
+}
