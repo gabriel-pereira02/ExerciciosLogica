58be9b7eaa070595576faa2a75c7b5290822e5f2
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-08 16:37:10 -0300

    Adicionado Ex01 de estrutura de repetição

---
+++ EstruturaRepeticao/Ex01/Ex01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros você vai digitar = ");
+            int vezes = int.Parse(Console.ReadLine());
+            int soma = 0;
+            for (int i = 1; i <= vezes; i++)
+            {
+                Console.Write($"Valor #{i}: ");
+                int valor = int.Parse(Console.ReadLine());
+                soma = soma + valor;
+            }
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
