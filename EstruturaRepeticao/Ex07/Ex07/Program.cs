2f6de52bc3d542ec88cadecb978b0f5fb85f11a6
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-08 17:19:50 -0300

    Adiciona Ex07

---
+++ EstruturaRepeticao/Ex07/Ex07/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool = 0;
+            int diesel = 0;
+            int gasolina = 0;
+            int opt = int.Parse(Console.ReadLine());
+            while (opt != 4)
+            {
+                if (opt == 1)
+                {
+                    alcool++;
+                }
+                else if (opt == 2)
+                {
+                    gasolina++;
+                }
+                else if (opt == 3)
+                {
+                    diesel++;
+                }
+
+                opt = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"MUITO OBRIGADO\nAlcool: {alcool}\nGasolina: {gasolina}\nDiesel: {diesel}");
+
+        }
+    }
+}
