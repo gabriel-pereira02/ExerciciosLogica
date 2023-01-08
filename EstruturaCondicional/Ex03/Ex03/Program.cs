7d223a28c6a1e26d92c10317f5e91ef9e09c171a
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-08 17:45:29 -0300

    Adiciona Ex03

---
+++ EstruturaCondicional/Ex03/Ex03/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Calcula o preço total de produtos pedidos pelo usuário
+             */
+            string[] produtos = Console.ReadLine().Split();
+            int codigo = int.Parse(produtos[0]);
+            int quantidade = int.Parse(produtos[1]);
+            float total = 0;
+
+            switch(codigo)
+            {
+                case 1:
+                    total = quantidade * 4;
+                    break;
+                case 2:
+                    total = quantidade * 4.50f;
+                    break;
+                case 3:
+                    total = quantidade * 5;
+                    break;
+                case 4:
+                    total = quantidade * 2;
+                    break;
+                case 5:
+                    total = quantidade * 1.50f;
+                    break;
+            }
+            Console.WriteLine($"TOTAL: R${total.ToString("F2")}");
+        }
+    }
+}
