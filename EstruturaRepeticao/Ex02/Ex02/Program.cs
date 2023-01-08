19bc909bce894e928fbb0162a6c6c428f82899c0
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-08 16:45:11 -0300

    Adiciona Ex02 de estrutura de repetição

---
+++ EstruturaRepeticao/Ex02/Ex02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Pede para o usuário digitar um número, imprimindo na tela todos os números ímpares até o número digitado 
+            */
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= x; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
