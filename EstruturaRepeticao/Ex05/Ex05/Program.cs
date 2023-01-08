bff5c5179249f073278435aa3be522c2d65018d4
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-08 16:58:57 -0300

    Adiciona Ex05

---
+++ EstruturaRepeticao/Ex05/Ex05/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] arr = Console.ReadLine().Split();
+                float n1 = int.Parse(arr[0]);
+                float n2 = int.Parse(arr[1]);
+                if(n2 == 0)
+                {
+                    Console.WriteLine("Divisão Impossível");
+                }
+                else
+                {
+                    Console.WriteLine((n1 / n2).ToString("F2"));
+                }
+            }
+        }
+    }
+}
