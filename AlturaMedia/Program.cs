d3c2f4ef86afb715d672241d906abae432859340
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-16 13:49:02 -0300

    Adiciona exercício de vetor que calcula a altura média de um grupo

---
+++ AlturaMedia/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AlturaMedia
+{
+    class Program{
+        static void Main(string[] args){
+            int numPessoas = int.Parse(Console.ReadLine());
+            double[] alturas = new double[numPessoas];
+            for(int i = 0; i < numPessoas; i++){
+                double altura = double.Parse(Console.ReadLine());
+                alturas[i] = altura;
+            }
+
+            double mediaAltura = (alturas.Sum())/numPessoas;
+            Console.WriteLine($"Altura média: {mediaAltura.ToString("F2")}");
+        }
+    }
+}
