dfdcae8fa0513efd87e4f05c0df35fdf3ff3a2d5
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-16 15:08:18 -0300

    Adiciona exercicio de vetores

---
+++ MediaPrecosProdutos/Produto.cs
@@ -0,0 +1,13 @@
+namespace MediaPrecosProdutos
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }     
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+    }
+}
---
+++ MediaPrecosProdutos/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MediaPrecosProdutos
+{
+    class Program{
+        static void Main(string[] args){
+            int qtdProdutos = int.Parse(Console.ReadLine());
+            Produto[] produtos = new Produto[qtdProdutos];
+            double soma = 0;
+            for(int i = 0; i < qtdProdutos; i++){
+                Console.Write("Nome do produto: ");
+                string nome = Console.ReadLine();
+                Console.Write("Preco do produto: ");
+                double preco = double.Parse(Console.ReadLine());
+                produtos[i]= new Produto(nome, preco);
+
+                soma += produtos[i].Preco;
+            }
+
+            double media = soma/qtdProdutos;
+            Console.WriteLine($"Média de preço produtos = {media}");
+        }
+    }
+}
