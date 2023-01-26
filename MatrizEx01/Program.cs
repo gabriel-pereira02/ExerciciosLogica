ef251a0d52a6116e6fc794b3075804ab23df644a
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-26 15:14:54 -0300

    Adiciona Ex01 de Matrizes

---
+++ MatrizEx01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MatrizEx01{
+    class Program{
+        static void Main(string[] args){
+            Console.Write("Digite a ordem da matriz: ");
+            int ordem = int.Parse(Console.ReadLine());
+            double[,] mat = new double[ordem, ordem];
+            int numNeg = 0;
+            string diag = "";
+            for(int i = 0; i < mat.GetLength(0); i++){
+                for(int o = 0; o < mat.GetLength(1); o++){
+                    Console.Write($"Digite o valor [{i}, {o}]: ");
+                    mat[i, o] = double.Parse(Console.ReadLine());
+                    if(mat[i, o] < 0){
+                        numNeg++;
+                    }
+
+                    if(i == o){
+                        diag += $"{mat[i,o].ToString()} ";
+                    }
+                     
+                }
+            }
+
+            Console.WriteLine($"Diagonal Principal: {diag}, números negativos na matriz: {numNeg}");
+
+        }
+    }
+}
